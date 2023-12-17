75aad5505eb74a46438d193a255fce5e0ffb682e
rohanraghuram <67184059+rohanraghuram@users.noreply.github.com>
2023-12-18 02:50:00 +0530

    Add files via upload

---
+++ event-bus/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import logger from 'morgan';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+const servicePorts = [
+  { name: 'players', port: 4000 },
+  { name: 'games', port: 4001 },
+  { name: 'query', port: 4002 },
+];
+
+
+app.post('/events', async (req, res) => {
+  const event = req.body;
+
+  console.log(`(${process.pid}) Event Bus (Received Event) ${event.type}`);
+
+  for (const { name, port } of servicePorts) {
+    try {
+      console.log(
+        `(${process.pid}) Event Bus (Sending Event to ${port}) ${event.type}`
+      );
+
+      await fetch(`http://${name}:${port}/events`, {
+        method: 'POST',
+        body: JSON.stringify(event),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log(`(${process.pid}) Event Bus Listening on 4005`);
+});
